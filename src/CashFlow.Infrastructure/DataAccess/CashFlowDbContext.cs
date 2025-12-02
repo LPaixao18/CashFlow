6bab66bfc084c261b36c06c2d55e5c0755282146
LPaixao18 <lara.paixao.nascimento@gmail.com>
2025-12-01 23:52:14 -0300

    Configurando MySQL e criando DbContext

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess;
+
 public class CashFlowDbContext : DbContext
 {
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
@@ -9,10 +10,14 @@ public class CashFlowDbContext : DbContext
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
+    //Por onde esta o banco de dados para o DbContext saber , meu optionsBuilder.UseMySql(""); vai receber 2 parametros , a connectionString e o ServerVersion
+    //Version(8, 0, 33)); os 3 numeros que aparecem na sessao Version do MySql 
+    //(Sintaxe do MySql o Sql Server é diferente) ConnectionString: "server=localhost;Database=cashflowdb;Uid=root;Pwd=@060523Li;"
 
-        modelBuilder.Entity<Tag>().ToTable("Tags");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=localhost;port=3307;Database=cashflowdb;Uid=root;Pwd=@060523Li;";
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 44));
+        optionsBuilder.UseMySql(connectionString, serverVersion);
     }
 }
